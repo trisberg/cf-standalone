6122eb5ccce603e18dbc3a48e89e7c2cfcba29ec
Thomas Risberg <trisberg@vmware.com>
2012-06-29 12:22:38 -0400

    changed maven config for standalone

---
+++ simple-spring/src/test.sh
@@ -0,0 +1,2 @@
+echo "Testing ..."
+java $JAVA_OPTS -cp spring-utility-1.0.0.CI-SNAPSHOT.jar:lib/* com.springdeveloper.demo.HelloSpring
--- simple-spring/test.sh
+++
@@ -1,2 +0,0 @@
-echo "Testing ..."
-java $JAVA_OPTS -jar ./target/spring-utility-1.0.0.CI-SNAPSHOT.jar 
